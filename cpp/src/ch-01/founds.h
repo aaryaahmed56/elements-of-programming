44c872f1d77b0b2d1720000d53bcae026490a2c2
Asef Ahmed <asefahmed56@gmail.com>
2019-11-11 23:10:19 -0500

    init

---
+++ cpp/src/ch-01/founds.h
@@ -0,0 +1,13 @@
+#include "../intrinsics.h"
+
+// Variadic template struct for equality.
+// Checks pair-wise.
+template< typename _Tp, typename... Args >
+    requires(Regular(_Tp))
+struct equal
+{
+    bool operator()(const _Tp& x, const _Tp& y, const Args&&... args)
+    {
+        return (x == y) && operator()(args...);
+    }
+};
