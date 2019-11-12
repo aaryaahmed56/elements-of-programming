68340ab6db9f6602aebdfe0fcb3801c56b12679f
Asef Ahmed <asefahmed56@gmail.com>
2019-11-11 23:10:01 -0500

    restruct

--- cpp/src/intrinsics.h
+++ cpp/src/intrinsics.h
@@ -9,13 +9,11 @@ namespace EOP
     // Template constraints
 
     // This treats the requires clause as documentation only.
-    
     #define requires(...)
 
     #define pointer(_Tp) _Tp*
 
     // Return addresses.
-
     template< typename _Tp >
     pointer(_Tp) address_of(_Tp& x)
     {
@@ -23,7 +21,6 @@ namespace EOP
     }
 
     // In-place construction and destruction.
-
     template < template< typename, typename... > class ContainerType,
                typename _Tp, typename... Args >
     void construct(const ContainerType<_Tp, Args...>& p)
@@ -72,7 +69,7 @@ namespace EOP
         {
                 // Precondition: $v$ is in a partially-formed state
                 // Postcondition: $v$ refers to raw memory, not an object
-                // Override $\func{destroy}$ to specialize destruction of a part of a container
+                // Override $\func{destruct}$ to specialize destruction of a part of a container
             destruct(v);
         }
     }
