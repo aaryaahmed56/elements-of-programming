496a90625fe00bb83ddfbc4f80b74099347e1aca
Asef Ahmed <asefahmed56@gmail.com>
2020-02-13 18:47:18 -0500

    modify equality, doxygening

--- cpp/src/ch-01/founds.hpp
+++ cpp/src/ch-01/founds.hpp
@@ -3,8 +3,15 @@
 
 #include "../intrinsics.hpp"
 #include "../concepts.hpp"
+
 namespace eop
 {
+/**
+ * @brief Copy and move construction semantics
+ * 
+ * @tparam _Tp0 A copy or move constructible type
+ * @tparam _Tp1 Another copy or move constructible type
+ */
 template< CopyConstructible _Tp0, CopyConstructible _Tp1 >
 struct copy_constructor
 {
@@ -23,6 +30,12 @@ struct move_constructor
     }
 };
 
+/**
+ * @brief Copy and move assignment semantics
+ * 
+ * @tparam _Tp0 A copy or move assignable type
+ * @tparam _Tp1 Another copy or move assignable type
+ */
 template< CopyAssignable _Tp0, CopyAssignable _Tp1 >
 struct copy_assignment
 {
@@ -41,14 +54,48 @@ struct move_assignment
     }
 };
 
-template< Regular _Tp, Regular... Args >
+/**
+ * @brief Variadic template for equality comparison
+ * 
+ * @tparam _Tp0 A regular type
+ * @tparam _Tp1 Another regular type, initialized by
+ * default as _Tp0
+ * @tparam Args Other regular types
+ */
+template< Regular _Tp0, Regular _Tp1 = _Tp0, Regular... Args >
 struct equal
 {
-    bool operator()(const _Tp& x, const _Tp& y, const Args&&... args)
+    inline
+    bool operator()(const _Tp0& x, const _Tp0& y, const Args&&... args) const
+    {
+        return (x == y) && operator()(args...);
+    }
+    inline
+    bool operator()(const _Tp0& x, const _Tp1& y, const Args&&... args) const
+    {
+        return (x == y) && operator()(args...);
+    }
+    inline
+    bool operator()(const _Tp1& x, const _Tp0& y, const Args&&... args) const
+    {
+        return (x == y) && operator()(args...);
+    }
+    inline
+    bool operator()(const _Tp1& x, const _Tp1& y, const Args&&... args) const
     {
         return (x == y) && operator()(args...);
     }
 };
+template< Regular _Tp >
+struct equal<_Tp>
+{
+    inline
+    bool operator()(const _Tp& x, const _Tp& y) const
+    {
+        return x == y;
+    }
+};
+
 } // namespace eop
 
 #endif // !EOP_CH1_FOUNDATIONS_H
